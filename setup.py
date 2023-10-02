68306463eef953d4a4c47b062b6a08d550db79d7
Balla Vamsi Srinivas <ballavamsisrinivas@gmail.com>
2023-10-02 13:19:44 +0530

    Added long description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='PyPushBullet',
     version='0.2.1',
     description='A simple library to push notification using PushBullet. You need a pushbullet API key for this',
+    long_description=open('README.md').read(),
     author='Balla Vamsi Srinivas',
     author_email='ballavamsisrinivas@gmail.com',
     url='https://github.com/ballavamsi/PyPushBullet',
